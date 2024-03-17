09b4cf775c989f6b47fcca64a492860f36529cf1
kiran <kirannadimpalli2000@gmail.com>
2024-03-17 16:56:25 +0530

    qr code

---
+++ qr_code_generator.py
@@ -0,0 +1,18 @@
+import qrcode
+
+link = "https://kirannadimpalli.github.io/product_images/main.html?image=image1.png"
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(link)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+
+img.save("qr_code.png")
+
+print("QR Code generated successfully.")
